3f17d54ece995e96638f3bcddbcd741641ca159b
Jason Jones <jsjones96@gmail.com>
2014-11-03 23:02:29 +0000

    basic mongodb config/setup w/ mongoose and mongolab.com

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,5 @@
 module.exports = {
     // development configuration options
+    db: 'mongodb://mean-book:mean-book@ds033390.mongolab.com:33390/mean-book',
     sessionSecret: 'developmentSessionSecret'
 };
