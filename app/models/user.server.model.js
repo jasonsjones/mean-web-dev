313174143a287c8e92a600bdac097fe12fac8709
Jason Jones <jsjones96@gmail.com>
2014-11-20 15:45:25 +0000

    add website property to user model
    Working through the next step in the project, this adds a website
    property to the user model and adds a custom getter modifier.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,12 +5,36 @@ var UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
+
+    website: {
+        type: String,
+        get: function (url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 &&
+                    url.indexOf('https://') !== 0) {
+
+                    url = 'http://' + url;
+
+                }
+                return url;
+            }
+        }
+    },
+
+    username: {
+        type: String,
+        trim: true
+    },
     password: String,
+
     created: {
         type: Date,
         default: Date.now
     }
 });
+
+UserSchema.set('toJSON', { getters: true });
 
 mongoose.model('User', UserSchema);
