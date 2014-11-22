590790e1879a1fd8841bdfb4b94a3187371ac7fc
Jason S. Jones <jsjones96@gmail.com>
2014-11-22 07:15:14 -0800

    remove hard-coded db connection credentials
    The mongolab db user creditials were hard-coded in the developmeent.js
    config file, which was accessible to the public on github.  Even though
    this is code only follows along with the book, I thought it would be a
    good idea to refactor the credentials to environment variables and
    source those to the operating system.
    
    The db user creditials have been moved to a separate file which has been
    added to .gitignore

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
     // development configuration options
-    db: 'mongodb://mean-book:mean-book@ds033390.mongolab.com:33390/mean-book',
+    db: 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PWD + 
+            '@ds033390.mongolab.com:33390/mean-book',
     sessionSecret: 'developmentSessionSecret'
 };
