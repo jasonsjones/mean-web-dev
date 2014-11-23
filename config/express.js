f521cc1e59233c363e4d2694bfb1073719d0a728
Jason S. Jones <jsjones96@gmail.com>
2014-11-22 20:58:03 -0800

    Install passport for authentication
    This commit begins chapter 6. We install passport and wire it up to
    use in express.

--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ var compress = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('express-session');
+var passport = require('passport');
 
 module.exports = function() {
     var app = express();
@@ -27,6 +28,9 @@ module.exports = function() {
 
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     require('../app/routes/index.server.routes')(app);
     require('../app/routes/users.server.routes')(app);
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
+var passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
+var passport = passport();
+
 var port = (process.env.PORT || 3000);
+
 app.listen(port);
+
+module.exports = app;
 
 console.log('Server running on port ' + port);
