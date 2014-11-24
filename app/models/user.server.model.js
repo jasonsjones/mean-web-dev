7ba996db9ea4b030340004f322ae93b8dfc319e1
Jason S. Jones <jsjones96@gmail.com>
2014-11-24 05:24:11 -0800

    modify user model to support passport

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -17,7 +17,27 @@ var UserSchema = new Schema({
         trim: true
     },
 
-    password: String,
+    password: {
+        type: String,
+        validate: [
+            function(password) {
+                return password && password.length > 6;
+            }, 'Password should be longer'
+        ]
+    },
+
+    salt: {
+        type: String
+    },
+
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+
+    providerId: String,
+
+    providerData: {},
 
     created: {
         type: Date,
@@ -32,6 +52,41 @@ UserSchema.virtual('fullName').get(function () {
     this.firstName = splitName[0] || '';
     this.lastName = splitName[1] || '';
 });
+
+UserSchema.pre('save', function (next) {
+    if (this.password) {
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+
+    next();
+});
+
+UserSchema.methods.hashPassword = function (password) {
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function (password) {
+    return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
+
+    _this.findOne({ username: possibleUsername }, function (err, user) {
+
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
+};
 
 UserSchema.set('toJSON', {
     getters: true,
