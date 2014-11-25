e512d2ef71bea5d0e1cb1c12b462cb1985f9b142
Jason S. Jones <jsjones96@gmail.com>
2014-11-25 05:19:32 -0800

    modify user ctrl to support passport

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,4 +1,87 @@
 var User = require('mongoose').model('User');
+var passport = require('passport');
+
+var getErrorMessage = function (err) {
+    var message = '';
+
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists';
+                break;
+            default:
+                message = 'Something went wrong';
+        }
+
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) {
+                message = err.errors[errName].message;
+            }
+        }
+    }
+
+    return message;
+
+};
+
+exports.renderSignin = function (req, res, next) {
+    if (!req.user) {
+        res.render('signin', {
+            title: 'Sign-in Form',
+            messages: req.flash('error') || req.flash('info')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.renderSignup = function (req, res, next) {
+    if (!req.user) {
+        res.render('signup', {
+            title: 'Sign-up Form',
+            messages: req.flash('error')
+        });
+    } else {
+        return res.redirect('/');
+    }
+};
+
+exports.signup = function (req, res, next) {
+    if (!req.user) {
+        var user = new User(req.body);
+        var message = null;
+
+        user.provider = 'local';
+
+        user.save(function (err) {
+            if (err) {
+                var message = getErrorMessage(err);
+
+                req.flash('error', message);
+                return res.redirect('/signup');
+            }
+
+            req.login(user, function(err) {
+                if (err) {
+                    return next(err);
+                }
+
+                return res.redirect('/');
+            });
+        });
+
+    } else {
+        return res.redirect('/');
+
+    }
+};
+
+exports.signout = function (req, res) {
+    req.logout();
+    res.redirect('/');
+};
 
 exports.create = function(req, res, next) {
     var user = new User(req.body);
