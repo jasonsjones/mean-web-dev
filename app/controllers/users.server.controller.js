f94506be6c226f9aea31390c82120e5b10dc590c
Jason S. Jones <jsjones96@gmail.com>
2014-11-26 05:57:03 -0800

    add saveOAuthUserProfile fn to user controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,7 +1,44 @@
 var User = require('mongoose').model('User');
 var passport = require('passport');
 
-var getErrorMessage = function(err) {
+exports.saveOAuthUserProfile = function (req, profile, done) {
+    User.findOne({
+
+        provider: profile.provider,
+        providerId: profile.providerId
+        
+    }, function (err, user) {
+       if (err) {
+           return done(err);
+       } else {
+           if (!user) {
+               var possibleUsername = profile.username || 
+                   ((profile.email) ? profile.email.split('@')[0] : '');
+
+                User.findUniqueUsername(possibleUsername, null, function (availableUsername) {
+                    profile.username = availableUsername;
+
+                    user = new User(profile);
+
+                    user.save(function (err) {
+                        if (err) {
+                            var message = _this.getErrorMessage(err);
+
+                            req.flash('error', message);
+                            return res.redirect('/signup');
+                        }
+
+                        return done(err, user);
+                    });
+                });
+           } else {
+            return done(err, user);
+           }
+       } 
+    });
+};
+
+var getErrorMessage = function (err) {
     var message = '';
 
     if (err.code) {
@@ -26,7 +63,7 @@ var getErrorMessage = function(err) {
 
 };
 
-exports.renderSignin = function(req, res, next) {
+exports.renderSignin = function (req, res, next) {
     if (!req.user) {
         res.render('signin', {
             title: 'Sign-in Form',
@@ -37,7 +74,7 @@ exports.renderSignin = function(req, res, next) {
     }
 };
 
-exports.renderSignup = function(req, res, next) {
+exports.renderSignup = function (req, res, next) {
     if (!req.user) {
         res.render('signup', {
             title: 'Sign-up Form',
@@ -48,7 +85,7 @@ exports.renderSignup = function(req, res, next) {
     }
 };
 
-exports.signup = function(req, res, next) {
+exports.signup = function (req, res, next) {
     if (!req.user) {
         var user = new User(req.body);
         var message = null;
@@ -63,7 +100,7 @@ exports.signup = function(req, res, next) {
                 return res.redirect('/signup');
             }
 
-            req.login(user, function(err) {
+            req.login(user, function (err) {
                 if (err) {
                     return next(err);
                 }
@@ -78,15 +115,15 @@ exports.signup = function(req, res, next) {
     }
 };
 
-exports.signout = function(req, res) {
+exports.signout = function (req, res) {
     req.logout();
     res.redirect('/');
 };
 
-exports.create = function(req, res, next) {
+exports.create = function (req, res, next) {
     var user = new User(req.body);
 
-    user.save(function(err) {
+    user.save(function (err) {
         if (err) {
             return next(err);
         } else {
@@ -95,8 +132,8 @@ exports.create = function(req, res, next) {
     });
 };
 
-exports.list = function(req, res, next) {
-    User.find({}, function(err, users) {
+exports.list = function (req, res, next) {
+    User.find({}, function (err, users) {
         if (err) {
             return next(err);
         } else {
@@ -105,12 +142,12 @@ exports.list = function(req, res, next) {
     });
 };
 
-exports.read = function(req, res) {
+exports.read = function (req, res) {
     res.json(req.user);
 };
 
-exports.delete = function(req, res, next) {
-    req.user.remove(function(err) {
+exports.delete = function (req, res, next) {
+    req.user.remove(function (err) {
         if (err) {
             return next(err);
         } else {
@@ -119,8 +156,8 @@ exports.delete = function(req, res, next) {
     });
 };
 
-exports.userByID = function(req, res, next, id) {
-    User.findOne({ _id: id }, function(err, user) {
+exports.userByID = function (req, res, next, id) {
+    User.findOne({ _id: id }, function (err, user) {
         if (err) {
             return next(err);
         } else {
@@ -130,8 +167,8 @@ exports.userByID = function(req, res, next, id) {
     });
 };
 
-exports.update = function(req, res, next) {
-    User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+exports.update = function (req, res, next) {
+    User.findByIdAndUpdate(req.user.id, req.body, function (err, user) {
         if (err) {
             return next(err);
         } else {
