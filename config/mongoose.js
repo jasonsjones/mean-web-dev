59d340f232a2ce5ab0d7879dd0cd4d64c115b194
Jason Jones <jsjones96@gmail.com>
2014-12-01 23:46:50 +0000

    add article model

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,6 +5,7 @@ module.exports = function() {
     var db = mongoose.connect(config.db);
 
     require('../app/models/user.server.model');
+    require('../app/models/article.server.model');
 
     return db;
 };
