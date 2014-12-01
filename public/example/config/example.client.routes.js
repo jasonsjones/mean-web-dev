f56b346c7c34c416a098dec1a85ee32731e38a11
Jason S. Jones <jsjones96@gmail.com>
2014-11-30 21:28:22 -0800

    Finish code for chapter 7
    The code in the book calls for a separate file to define the 'users'
    angular module then list in index.ejs, which makes sense.  However, when
    that is done, the system shows a 500 internal server error with some
    information in the stacktrace about a mongoose cast error, which makes
    no sense.  Why does mongoose, or mongodb, have anything to do with
    sourcing a simple javascript file in an html page.
    
    This issue is resolved however when the angular module definition is
    moved to the service (factory) file in a subdirectory.  I don't get it...

---
+++ public/example/config/example.client.routes.js
@@ -0,0 +1,8 @@
+angular.module('example').config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'example/views/example.client.view.html'
+        })
+        .otherwise({ redirectTo: '/' });
+}
+]);
