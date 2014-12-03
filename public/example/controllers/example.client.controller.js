9fabc26507297337d7a9c60accfefc01913025c0
Jason S. Jones <jsjones96@gmail.com>
2014-12-03 06:08:01 -0800

    finalize implementation of article module
    This finishes up the code for chapter 8 of the book.

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,6 +1,6 @@
 angular.module('example')
     .controller('ExampleController', 
         ['$scope', 'Authentication', function($scope, Authentication) {
-            $scope.name = Authentication.user ? Authentication.user.fullName: 'MEAN Application';
+            $scope.authentication = Authentication;
         }
     ]);
