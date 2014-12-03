717c7adf37a92a6c68d5e0a710f37e060ccdbb30
Jason S. Jones <jsjones96@gmail.com>
2014-12-03 06:06:58 -0800

    add article module and routes

---
+++ public/articles/config/articles.client.routes.js
@@ -0,0 +1,18 @@
+angular.module('articles')
+    .config(['$routeProvider', 
+        function ($routeProvider) {
+            $routeProvider
+                .when('/articles', {
+                    templateUrl: 'articles/views/list-articles.client.view.html'
+                })
+                .when('/articles/create', {
+                    templateUrl: 'articles/views/create-article.client.view.html'
+                })
+                .when('/articles/:articleId', {
+                    templateUrl: 'articles/views/view-articles.client.view.html'
+                })
+                .when('/articles/:articleId/edit', {
+                    templateUrl: 'articles/views/edit-articles.client.view.html'
+                });
+        }
+    ]);
